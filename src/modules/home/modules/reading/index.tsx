ff9d1b28fa5dfe23a01b605749b90d855cb170f5
nguyenconghieudata <hieunc@farmcode.io.vn>
2025-05-21 14:32:22 +0700

    add: test CICD 2

--- src/modules/home/modules/reading/index.tsx
+++ src/modules/home/modules/reading/index.tsx
@@ -82,7 +82,7 @@ export default function Reading() {
           <div className="flex items-center flex-1">
             <h5>
               <span className="text-gray-800 text-[20px] font-bold">
-                DANH SÁCH BÀI ĐỌC hohoho{" "}
+                DANH SÁCH BÀI ĐỌC kekekekeke{" "}
                 <span className="text-indigo-600">({data?.length})</span>
               </span>
             </h5>
